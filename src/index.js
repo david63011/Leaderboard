35d73104889b11bc677eeb55c57d481d67373456
david63011 <david630@gmail.com>
2022-10-25 20:28:45 -0300

    Add changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-/* eslint-disable import/extensions */
 /* eslint-disable import/no-cycle */
 /* eslint-disable import/prefer-default-export */
-/* eslint-disable no-use-before-define */
-/* eslint-disable max-classes-per-file */
 import './styles/main.scss';
-import saveScore from './modules/save.js';
-import { renderScores } from './modules/render.js';
+import saveScore from './modules/save';
+import { renderScores } from './modules/render';
 
 const form = document.querySelector('.form');
 export const scores = JSON.parse(localStorage.getItem('scores')) || [];
